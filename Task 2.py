31c3a9e3404691734d426ff78c329572a52bd0b5
DHARMENDRA PRASAD <161959278+Tech-Dharm95@users.noreply.github.com>
2025-07-29 16:50:51 +0530

    Add files via upload

---
+++ Task 2.py
@@ -0,0 +1,8 @@
+import math
+n = int(input("Enter a number: "))
+if n > 0:
+    print("Square root: ",math.sqrt(n))
+    print("Logarithm:  ", math.log(n))
+else:
+    print("Enter Positive number for Square root,natural log")
+print('sine: ',math.sin(n))
